c3931e8e66a41fa7409cddbe9e91cbe4c1e0bb3c
David Tran <isorunner@yahoo.com>
2015-06-15 21:12:34 -0700

    added env variables

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,13 +1,57 @@
 require 'capybara/cucumber'
 require 'capybara'
 require 'rspec'
-require 'factory_girl'
 require 'capybara/poltergeist'
 require 'selenium-webdriver'
 require 'cucumber'
 
-def browser_name
-  (ENV['BROWSER']) ||= 'firefox'.downcase.to_sym
+if ENV['chrome']
+  Capybara.default_driver = :chrome
+  Capybara.register_driver :chrome do |app|
+    options = {
+        :js_errors => false,
+        :timeout => 360,
+        :debug => false,
+        :inspector => false,
+    }
+#    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+    Capybara::Selenium::Driver.new(app, :browser => :chrome, :switches => ["--load-extension=features/support/Extensions/Chrome/2.3.19_0/"])
+  end
+
+elsif ENV['firefox']
+  profile = Selenium::WebDriver::Firefox::Profile.new
+  profile.add_extension "features/support/Extensions/FireFox/2_2_24/onelogin.xpi"
+  Capybara.default_driver = :firefox
+  Capybara.register_driver :firefox do |app|
+    options = {
+        :js_errors => true,
+        :timeout => 360,
+        :debug => false,
+        :inspector => false,
+    }
+    Capybara::Selenium::Driver.new(app, :browser => :firefox, :profile => profile)
+  end
+elsif ENV['iexplore']
+  Capybara.default_driver = :selenium
+  Capybara.register_driver :selenium do |app|
+    options = {
+        :js_errors => true,
+        :timeout => 360,
+        :debug => false,
+        :inspector => false,
+    }
+    Capybara::Selenium::Driver.new(app, :browser => :internet_explorer)
+  end
+
+elsif ENV['safari']
+  Capybara.default_driver = :safari
+  Capybara.register_driver :safari do |app|
+    options = {
+        :js_errors => false,
+        :timeout => 360,
+        :debug => false,
+        :inspector => false,
+    }
+    Capybara::Selenium::Driver.new(app, :browser => :safari)
+  end
 end
-
-
